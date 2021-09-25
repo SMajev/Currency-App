ab34091b80688b53b33c956e20de5addb17234ed
smajev <smajev@gmail.com>
2021-09-25 16:59:06 +0200

    new func pln rate

--- root.py
+++ root.py
@@ -5,15 +5,30 @@ import numpy as np
 
 class root:
     def __init__(self) -> None:
-        self.nbp = nbp_api.NBPAPI
+        self._nbp = nbp_api.NBPAPI
+
+
+    def get_pln_rate(self) -> str:
+        rate = input("Względem jakiej waluty chcesz otrzymać kurs: ").upper()
+        result = self._nbp.get_rate_to_pln(rate)
+        return f'1 PLN --> {result} {rate}'
+
+    def get_current_rate(self):
+        rate_a = input("Wpisz symbol waluty, na którą chcesz przeliczyć: ")
+        rate_b = input("Wpisz symbol waluty która posiadasz: ")
+        rate_a_value = self._nbp.get_rate_to_pln(rate_a)
+        rate_b_value = self._nbp.get_rate_to_pln(rate_a)
+        result = round(rate_a_value / rate_b_value, 4)
+        return f"\n{result} {rate_b} / 1 {rate_a}\n"
+
 
     def predict_to_tomorrow(self):
-        self.rates_lst = np.array(self.nbp.get_last_10_rates())
-        sns.scatterplot(self.rates_lst)
-        print(self.rates_lst)
+        self.rates_lst = np.array(self._nbp.get_last_10_rates('eur', 10))
+        return self.rates_lst
 
 
 
 if __name__ == '__main__':
     root = root()
-    root.predict_to_tomorrow()
+    print(root.predict_to_tomorrow())
+    print(root.get_pln_rate())
